31fb6a2109ad4218a9d84f27e8e51bacd4beb16f
Bryan Cabrera <bryangcr25@gmail.com>
2023-03-14 21:37:59 +0100

    Comment matplotlib lines.

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -1,5 +1,4 @@
 import cv2
-import matplotlib.pyplot as plt
 
 
 def photo_analyzer(filename, id_wanted, show=False, output=None):
@@ -34,21 +33,21 @@ def photo_analyzer(filename, id_wanted, show=False, output=None):
             trigger = True
 
     if show or output is not None:
-        plt.figure()
+        # plt.figure()
         # plt.imshow(frame_markers, origin="upper")
         if markerIds is not None:
             for i in range(len(markerIds)):
                 c = markerCorners[i][0]
-                plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "+", label="id={0}".format(markerIds[i]))
+                # plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "+", label="id={0}".format(markerIds[i]))
                 """for points in rejectedImgPoints:
                     y = points[:, 0]
                     x = points[:, 1]
                     plt.plot(x, y, ".m-", linewidth = 1.)"""
-        plt.legend()
-        if show:
-            plt.show()
-        if output is not None:
-            plt.savefig(output)
+        # plt.legend()
+        #if show:
+        #    # plt.show()
+        #if output is not None:
+        #    # plt.savefig(output)
 
     return trigger
 
