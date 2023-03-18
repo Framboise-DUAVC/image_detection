400e2b19a0773f3983e7dbd010aaf38d6d1ed14b
Bryan Cabrera <bryangcr25@gmail.com>
2023-03-18 16:22:39 +0100

    Fx issue.

--- photographer.py
+++ photographer.py
@@ -135,7 +135,7 @@ def main(args):
         try:
             for i, filename in enumerate(camera.capture_continuous(os.path.join(output, 'image_{counter:02d}.jpg'))):
                 # Build arguments
-                job1_args = (i, filename, 7, return_queue, show, filename.replace('.jpg', '-Analyzed.jpg'), verbose)
+                job1_args = (i, filename, 7, show, filename.replace('.jpg', '-Analyzed.jpg'), verbose)
 
                 # Launch process to evaluate image
                 jobs_return_dict[i] = worker_photo_analyzer(*job1_args)
