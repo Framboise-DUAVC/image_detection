d88643b737f0a734c2e4a5bcc7a720eb184554a6
Bryan Cabrera <bryangcr25@gmail.com>
2023-03-19 12:49:48 +0100

    Recover working version for pc.

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 
 def photo_analyzer(filename, id_wanted, show=False, output=None):
@@ -21,15 +22,15 @@ def photo_analyzer(filename, id_wanted, show=False, output=None):
         # Window shown waits for any key pressing event
         # Window shown waits for any key pressing event
         # Window shown waits for any key pressing event
-        
+
         cv2.destroyAllWindows()
 
     dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
-    parameters = cv2.aruco.DetectorParameters_create()
-    # detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+    parameters = cv2.aruco.DetectorParameters()
+    detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 
-    markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(gray_image, dictionary, parameters=parameters)
-    # frame_markers = cv2.aruco.drawDetectedMarkers(frame.copy(), markerCorners, markerIds)
+    markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(gray_image)
+    frame_markers = cv2.aruco.drawDetectedMarkers(frame.copy(), markerCorners, markerIds)
 
     if markerIds is not None:
         for id in markerIds:
@@ -37,21 +38,21 @@ def photo_analyzer(filename, id_wanted, show=False, output=None):
                 trigger = True
 
     if show or output is not None:
-        # plt.figure()
-        # plt.imshow(frame_markers, origin="upper")
+        plt.figure()
+        plt.imshow(frame_markers, origin="upper")
         if markerIds is not None:
             for i in range(len(markerIds)):
                 c = markerCorners[i][0]
-                # plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "+", label="id={0}".format(markerIds[i]))
+                plt.plot([c[:, 0].mean()], [c[:, 1].mean()], "+", label="id={0}".format(markerIds[i]))
                 """for points in rejectedImgPoints:
                     y = points[:, 0]
                     x = points[:, 1]
                     plt.plot(x, y, ".m-", linewidth = 1.)"""
-        # plt.legend()
-        #if show:
-        #    # plt.show()
-        #if output is not None:
-        #    # plt.savefig(output)
+        plt.legend()
+        if show:
+            plt.show()
+        if output is not None:
+            plt.savefig(output)
 
     return trigger
 
@@ -59,4 +60,4 @@ def photo_analyzer(filename, id_wanted, show=False, output=None):
 # TODO: remove this
 if __name__ == '__main__':
     # Call main function
-    photo_analyzer(filename="test/aruco_id7_grass.jpeg", id_wanted=7, show=True)
+    photo_analyzer(filename="/home/bryan/CLionProjects/ISAE/image_detection/test/out/photo_test_huge_aruco_cardboard/home/pi/photo_test_huge_aruco_cardboard/raspy_000000008.jpg", id_wanted=7, show=True)
