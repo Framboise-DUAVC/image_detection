4422991119fa10a98b8ecb5541db088805d9f532
Bryan Cabrera <bryangcr25@gmail.com>
2023-03-21 21:03:43 +0100

    Trial

--- on_y_va.py
+++ on_y_va.py
@@ -5,7 +5,7 @@ import mavsdk
 async def main():
     drone = mavsdk.System()
     print("Trying to connect to the drone...")
-    await drone.connect(system_address="serial:///dev/FAKE0")
+    await drone.connect(system_address="serial:///dev/FAKE1")
     print("Waiting for drone to connect...")
 
     async for state in drone.core.connection_state():
