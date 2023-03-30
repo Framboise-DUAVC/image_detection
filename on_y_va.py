f6c09c245cba10637eece572e51d2b3d6186510b
krishnamrl <krishna.murali@student.isae-supaero.fr>
2023-03-30 16:24:19 +0200

    Added gimbal command

--- on_y_va.py
+++ on_y_va.py
@@ -43,7 +43,7 @@ async def main():
     print(f"-- Aruco id n {7} detected!")
 
     # Trapdoor actuation and payload drop
-    await drone.gimbal.set_pitch_and_yaw(0,70) # args = (float pitch_deg, float yaw_deg)
+    await drone.gimbal.set_pitch_and_yaw(0,90) # args are (float pitch_deg, float yaw_deg)
     print("-- Trapdoor opening...")
     time.sleep(3)
     print("-- Payload dropped!")
