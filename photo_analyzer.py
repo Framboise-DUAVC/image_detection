8e577efad54d93f87b22dc3c5cfb2ab990d7c5fe
k-bhardwaj <kshitij.bhardwaj@student.isae-supaero.fr>
2023-04-03 21:05:51 +0200

    aruco_try

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -26,10 +26,12 @@ def photo_analyzer(filename, id_wanted, show=False, output=None):
 #imessed up the commit
     # imessed up the commit
     dictionary_main = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    
     #bits_str = dictionary_main['marker_7']
     #print(bits_str)
-    fs = cv2.FileStorage("/home/pi/dictionary.txt")
-    dict_str = dictionary_main.writeDictionary(fs)
+    #fs = cv2.FileStorage("/home/pi/dictionary.txt")
+    #dict_str = dictionary_main.writeDictionary(fs)
+    cv2.aruco.Dictionary.readDictionary(fn)
 
     # detector = cv2.aruco.ArucoDetector(dictionary, parameters)
 
