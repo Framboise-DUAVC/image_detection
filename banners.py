5484873bf8fa1b732123d6f955304081dc655704
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-04 15:21:51 +0200

    Fix bug.

--- banners.py
+++ banners.py
@@ -1,7 +1,7 @@
-
 def get_px4_banner():
-    return "PPPPPP  XX    XX     44      PPPPPP  HH   HH  OOOOO  EEEEEEE NN   NN IIIII XX    XX \n\
-            PP   PP  XX  XX     444      PP   PP HH   HH OO   OO EE      NNN  NN  III   XX  XX  \n\
-            PPPPPP    XXXX    44  4      PPPPPP  HHHHHHH OO   OO EEEEE   NN N NN  III    XXXX   \n\
-            PP       XX  XX  44444444    PP      HH   HH OO   OO EE      NN  NNN  III   XX  XX  \n\
-            PP      XX    XX    444      PP      HH   HH  OOOO0  EEEEEEE NN   NN IIIII XX    XX"
+    banner = "PPPPPP  XX    XX     44      PPPPPP  HH   HH  OOOOO  EEEEEEE NN   NN IIIII XX    XX \n"
+    banner += "PP   PP  XX  XX     444      PP   PP HH   HH OO   OO EE      NNN  NN  III   XX  XX  \n"
+    banner += "PPPPPP    XXXX    44  4      PPPPPP  HHHHHHH OO   OO EEEEE   NN N NN  III    XXXX   \n"
+    banner += "PP       XX  XX  44444444    PP      HH   HH OO   OO EE      NN  NNN  III   XX  XX  \n"
+    banner += "PP      XX    XX    444      PP      HH   HH  OOOO0  EEEEEEE NN   NN IIIII XX    XX"
+    return banner
