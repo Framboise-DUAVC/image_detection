e607d26bfa31fcc45416ba3328a001f2f27482b9
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-04 15:22:28 +0200

    Fix spaces.

--- banners.py
+++ banners.py
@@ -1,7 +1,9 @@
 def get_px4_banner():
-    banner = "PPPPPP  XX    XX     44      PPPPPP  HH   HH  OOOOO  EEEEEEE NN   NN IIIII XX    XX \n"
+    banner = "\n"
+    banner += "PPPPPP  XX    XX     44      PPPPPP  HH   HH  OOOOO  EEEEEEE NN   NN IIIII XX    XX \n"
     banner += "PP   PP  XX  XX     444      PP   PP HH   HH OO   OO EE      NNN  NN  III   XX  XX  \n"
     banner += "PPPPPP    XXXX    44  4      PPPPPP  HHHHHHH OO   OO EEEEE   NN N NN  III    XXXX   \n"
     banner += "PP       XX  XX  44444444    PP      HH   HH OO   OO EE      NN  NNN  III   XX  XX  \n"
     banner += "PP      XX    XX    444      PP      HH   HH  OOOO0  EEEEEEE NN   NN IIIII XX    XX"
+    banner += "\n"
     return banner
