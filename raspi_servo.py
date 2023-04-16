5c994145edc91068d64155953f899d998211a6d6
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-16 18:58:54 +0200

    Formatting.

--- raspi_servo.py
+++ raspi_servo.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 
-def command_servo(pin_place: int, frequency: float):
 
+def command_servo(pin_place: int, frequency: float):
     # Set everything up
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin_place, GPIO.OUT)
