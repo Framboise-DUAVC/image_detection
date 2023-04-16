8731e31349dda11c98c194cfa72858acd3b3029a
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-16 16:33:28 +0200

    Testing PWM...

--- trapdoor.py
+++ trapdoor.py
@@ -32,7 +32,7 @@ async def trapdoor_servo_actuator2(drone):
 
     # set control mode of servo to primary
     print("Taking control of trapdoor servo")
-    await drone.action.set_actuator(1, 0)
+    await drone.action.set_actuator(1, 1)
 
 
 async def print_gimbal_position(drone):
