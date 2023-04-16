097e865c6af31fef0da0ee0561d34f42632db63b
krishnamrl <krishna.murali@student.isae-supaero.fr>
2023-04-16 18:47:15 +0200

    raspi servo test.

---
+++ raspi_servo.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(6, GPIO.OUT)
+servo = GPIO.PWM(6, 50)
+
+servo.start(0)
+
+print("Rotating at intervals of 12 degrees")
+duty = 2
+while duty <= 17:
+    servo.ChangeDutyCycle(duty)
+    time.sleep(1)
+    duty = duty + 1
+
+print("Turning back to 0 degrees")
+servo.ChangeDutyCycle(2)
+time.sleep(1)
+servo.ChangeDutyCycle(0)
+
+servo.stop()
+GPIO.cleanup()
+print ("Everything's cleaned up")
