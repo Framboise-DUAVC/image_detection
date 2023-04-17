0d8178bcc66a3ad3eb1b48775a6a2aab8eadcfe2
krishnamrl <krishna.murali@student.isae-supaero.fr>
2023-04-17 21:46:17 +0200

    raspi servo script test.

--- raspi_servo_simple.py
+++ raspi_servo_simple.py
@@ -16,7 +16,7 @@ pin = 32
 # other pins: power = 17, ground = 20
 RPi.GPIO.setmode(RPi.GPIO.BOARD)
 RPi.GPIO.setup(pin, RPi.GPIO.OUT)
-pwm = RPi.GPIO.PWM(pin, 50)
+pwm = RPi.GPIO.PWM(pin, 30)
 pwm.start(0)
 sm_angle = 45
 set_angle(sm_angle, pin)
