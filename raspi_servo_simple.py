3ce3bc05053d32675664b2db389db12b362b4333
krishnamrl <krishna.murali@student.isae-supaero.fr>
2023-04-17 21:44:57 +0200

    raspi servo script test.

--- raspi_servo_simple.py
+++ raspi_servo_simple.py
@@ -1,21 +1,24 @@
 import RPi.GPIO
 import time
 
-pin = 32
-# other pins: power = 17, ground = 20
-RPi.GPIO.setmode(RPi.GPIO.BOARD)
-RPi.GPIO.setup(pin_no, RPi.GPIO.OUT)
-pwm = RPi.GPIO.PWM(pin_no, 50)
-pwm.start(0)
-sm_angle = 45
-actuate = SetAngle(sm_angle, pin)
-pwm.stop()
-RPi.GPIO.cleanup()
-def SetAngle(angle, pin_no):
+
+def set_angle(angle, pin_no):
     duty = (angle / 18) + 2
     RPi.GPIO.output(pin_no, True)
     pwm.ChangeDutyCycle(duty)
     time.sleep(1)
     RPi.GPIO.output(pin_no, False)
     pwm.ChangeDutyCycle(0)
+    return
 
+
+pin = 32
+# other pins: power = 17, ground = 20
+RPi.GPIO.setmode(RPi.GPIO.BOARD)
+RPi.GPIO.setup(pin, RPi.GPIO.OUT)
+pwm = RPi.GPIO.PWM(pin, 50)
+pwm.start(0)
+sm_angle = 45
+set_angle(sm_angle, pin)
+pwm.stop()
+RPi.GPIO.cleanup()
