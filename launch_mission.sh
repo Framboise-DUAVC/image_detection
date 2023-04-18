70899d99d3970d679fe5ba1c3d189c0c15492635
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-18 18:45:02 +0200

    Fix argument.

--- launch_mission.sh
+++ launch_mission.sh
@@ -1,4 +1,4 @@
 MAX_TIME="3600"
 RECEIVER="bryan@msipre.local"
 MISSION="true"
-bash embed_launch.sh -o "$HOME/camera_test" -r "${RECEIVER}" -mt "${MAX_TIME}" -mission "${MISSION}" -rm
+bash embed_launch.sh -o "$HOME/camera_test" -r "${RECEIVER}" -mt "${MAX_TIME}" --mission "${MISSION}" -rm
