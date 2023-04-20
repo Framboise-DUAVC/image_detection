28c9fd2c162eadf9b6eaab03897252a616ffeea0
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-20 20:44:47 +0200

    Fix bug.

--- detection_and_action_embedded.sh
+++ detection_and_action_embedded.sh
@@ -1,11 +1,2 @@
-
-# Screen username
-uname="detection_action-$1"
-mission_log="/home/pi/mission_log_${uname}.log"
-
-
-# Open screen session
-screen -S "${uname}"
-
 # Launch python command
 python3 detection_action_test.py | tee "${mission_log}"
