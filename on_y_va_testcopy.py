1b69599e7ffbe680667d1b18b7f33e2d72bfcf17
krishnamrl <krishna.murali@student.isae-supaero.fr>
2023-04-27 00:47:03 +0200

    fix bug.

--- on_y_va_testcopy.py
+++ on_y_va_testcopy.py
@@ -1,5 +1,7 @@
 import asyncio
 import mavsdk
+
+import Logger
 import banners
 import datetime
 
@@ -13,7 +15,7 @@ async def main(verbose: bool = True):
     drone = mavsdk.System()
 
     # Show info
-    tools.print_msg("Trying to connect...", verbose=verbose)
+    tools.simple_print_msg("Trying to connect...", verbose=verbose)
 
     # Drone is in serial0
     await drone.connect(system_address="serial:///dev/serial0:921600")
@@ -22,21 +24,41 @@ async def main(verbose: bool = True):
     status_text_task = asyncio.ensure_future(print_status_text(drone, verbose=verbose))
 
     # Info...
-    tools.print_msg("Waiting for drone to connect...", verbose=verbose)
+    tools.simple_print_msg("Waiting for drone to connect...", verbose=verbose)
 
     # Get connection state
     async for state in drone.core.connection_state():
         if state.is_connected:
             # Info if connected
-            tools.print_msg(f"-- Connected to drone!", verbose=verbose)
+            tools.simple_print_msg(f"-- Connected to drone!", verbose=verbose)
 
             # Show banner
-            tools.print_msg(f"{banners.get_px4_banner()}", verbose=verbose)
+            tools.simple_print_msg(f"{banners.get_px4_banner()}", verbose=verbose)
 
             # Exit async
             break
 
-    tools.print_msg(f"image captured", verbose=verbose)
+    # Info...
+    tools.simple_print_msg("-- Arming", verbose=verbose)
+
+    # Arm
+    await drone.action.arm()
+
+    # Get the flight mode
+    async for flight_mode in drone.telemetry.flight_mode():
+        # Display flight mode
+        tools.simple_print_msg(f"-- FlightMode: {str(flight_mode)}", verbose=verbose)
+
+    # Status check
+    status_text_task.cancel()
+
+
+async def print_status_text(drone, verbose: bool):
+    try:
+        async for status_text in drone.telemetry.status_text():
+            tools.simple_print_msg(f"Status: {status_text.type}: {status_text.text}", verbose=verbose)
+    except asyncio.CancelledError:
+        return
 
 
 if __name__ == '__main__':
