d84cb92d53e35ce594189de5fa8f390656bcfce4
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-26 20:20:17 +0200

    Refactor.

--- detection_and_action_embedded.sh
+++ detection_and_action_embedded.sh
@@ -1,6 +1,6 @@
 # Screen username
-uname="detection_action-$1"
-mission_log="/home/pi/mission_log_${uname}.log"
+mission_name="detection_action-$1"
+mission_log="/home/pi/mission_log_${mission_name}.log"
 
 # Launch python command
 python3 detection_action_test.py | tee "${mission_log}"
