c0bbfab56c663ef6a47d7fdff67a9136f94a06b1
Bryan Cabrera <bryangcr25@gmail.com>
2023-04-28 03:11:56 +0200

    some tests

--- photo_analyzer.py
+++ photo_analyzer.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
@@ -19,13 +19,25 @@ def photo_analyzer(filename, id_wanted, show=False, output=None, rotate=None):
         cv2.imshow('Original', frame)
         cv2.waitKey(0)
 
+    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    kernel = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
+    kernel = np.array([[-1,-1,-1],
+                       [-1, 9,-1],
+                       [-1,-1,-1]])
+    sharpened = cv2.filter2D(frame, -1, kernel)
+
+    if show:
+        cv2.imshow('Sharpened', sharpened)
+        cv2.waitKey(0)
+
+
     #frame = cv2.normalize(frame, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
     #if show:
     #    cv2.imshow('Normalized', frame)
     #    cv2.waitKey(0)
     #
     # Use the cvtColor() function to grayscale the image
-    gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_image = cv2.cvtColor(sharpened, cv2.COLOR_BGR2GRAY)
 
     if show:
         cv2.imshow('Grayscale', gray_image)
@@ -105,6 +117,145 @@ def main(general_path, id_wanted, rotate=None):
         photo_analyzer(general_path, id_wanted, True)
 
 
+def test():
+    img = cv2.imread(pathinput)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.threshold(gray, 70, 255, cv2.THRESH_BINARY)[1]
+    contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+    print("Number of contours detected:", len(contours))
+
+    for cnt in contours:
+        x1,y1 = cnt[0][0]
+        approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+        if len(approx) == 4:
+            x, y, w, h = cv2.boundingRect(cnt)
+            ratio = float(w)/h
+            if ratio >= 0.9 and ratio <= 1.1:
+                img = cv2.drawContours(img, [cnt], -1, (0,255,255), 3)
+                cv2.putText(img, 'Square', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 0), 2)
+            else:
+                cv2.putText(img, 'Rectangle', (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+                img = cv2.drawContours(img, [cnt], -1, (0,255,0), 3)
+
+    cv2.imshow("Shapes", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def test2():
+    # Load the image
+    img = cv2.imread(pathinput)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply a thresholding operation
+    thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
+
+    # Find the contours in the binary image
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Iterate over the contours and filter out non-box-like contours
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        aspect_ratio = float(w)/h
+        if aspect_ratio > 0.9 and aspect_ratio < 1.1:
+            # Draw a rectangle around the contour
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the image with boxes drawn around the white boxes
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def test3():
+    # Load the image
+    img = cv2.imread(pathinput)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply a thresholding operation
+    thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
+
+    # Find the contours in the binary image
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Iterate over the contours and filter out non-box-like contours
+    for contour in contours:
+        # Get the perimeter of the contour
+        perimeter = cv2.arcLength(contour, True)
+
+        # Approximate the contour as a polygon
+        approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+
+        # Check if the polygon has four sides (i.e., is roughly rectangular)
+        if len(approx) == 4:
+            # Fit a rotated rectangle around the contour
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+
+            # Compute the aspect ratio of the rectangle
+            w, h = rect[1]
+            aspect_ratio = float(w) / h if h != 0 else 0
+
+            # Check if the aspect ratio is within a certain range
+            if aspect_ratio > 0.9 and aspect_ratio < 1.1:
+                # Draw a rectangle around the contour
+                cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+
+    # Display the image with boxes drawn around the white boxes
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def test4():
+    # Load the image
+    img = cv2.imread(pathinput)
+
+    # Apply a bilateral filter to reduce noise and preserve edges
+    blurred = cv2.bilateralFilter(img, 9, 75, 75)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+
+    # Apply a thresholding operation
+    thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)[1]
+
+    # Find the contours in the binary image
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Iterate over the contours and filter out non-box-like contours
+    for contour in contours:
+        # Get the perimeter of the contour
+        perimeter = cv2.arcLength(contour, True)
+
+        # Approximate the contour as a polygon
+        approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+
+        # Check if the polygon has four sides (i.e., is roughly rectangular)
+        if len(approx) == 4:
+            # Fit a rotated rectangle around the contour
+            rect = cv2.minAreaRect(contour)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+
+            # Compute the aspect ratio of the rectangle
+            w, h = rect[1]
+            aspect_ratio = float(w) / h if h != 0 else 0
+
+            # Check if the aspect ratio is within a certain range
+            if aspect_ratio > 0.9 and aspect_ratio < 1.1:
+                # Draw a rectangle around the contour
+                cv2.drawContours(img, [box], 0, (0, 255, 0), 2)
+
+    # Display the image with boxes drawn around the white boxes
+    cv2.imshow('Image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 # TODO: remove this
 if __name__ == '__main__':
     # Some performed tests
@@ -118,7 +269,11 @@ if __name__ == '__main__':
     photo_test_huge_aruco_MISSION_1_photo2_mod2 = "/home/bryan/Downloads/mission_20230427_170253/modified/raspy_exp_id_0000000427_exp_2032_mod2.jpg"
 
     # Dir input
-    pathinput = photo_test_huge_aruco_MISSION_1_photo2_mod2
+    pathinput = photo_test_huge_aruco_MISSION_1_photo2
+
+    test4()
+
+    exit()
 
     # Call main function
     if os.path.isdir(pathinput):
